e0e011c1c3d63414806e82853ff3009e55e03c86
Neil Harlow <nharlow89@gmail.com>
2019-05-15 11:06:04 -0700

    fixed that previous test, tested on windows machine

--- test.js
+++ test.js
@@ -318,7 +318,7 @@ tape('cannot escape directory', function (t) {
 })
 
 tape('directory filename resolves correctly', function (t) {
-  var name = gen()
+  var name = 'test.txt'
   var file = raf(name, {writable: true, directory: tmp})
   t.same(file.filename, path.join(tmp, name))
   t.end()
