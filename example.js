ccd1df993cf7f5f40913a44aab0a616690c811bb
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-06-09 19:04:41 +0200

    Implement as class and port tests to Brittle (#27)
    * Implement as class and port tests to Brittle
    
    * Remove `mkdirp-classis`
    
    Co-authored-by: Feross Aboukhadijeh <feross@feross.org>
    
    * Export class
    
    * Use default params
    
    * Update example
    
    * No need for that
    
    * Ignore more dirs
    
    Co-authored-by: Feross Aboukhadijeh <feross@feross.org>

--- example.js
+++ example.js
@@ -1,11 +1,11 @@
-var raf = require('random-access-file')
-var file = raf('hello.txt')
+const RAF = require('.')
+const file = new RAF('hello.txt')
 
-var max = 500 * 1024 * 1024
-var buf = Buffer.alloc(1024)
+const max = 500 * 1024 * 1024
+const buf = Buffer.alloc(1024)
 buf.fill('lo')
 
-var offset = 0
+let offset = 0
 write()
 
 function write () {
