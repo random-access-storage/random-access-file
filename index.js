f0baecba2e24c38a56ffe173f9e3dea71661ee14
HDegroote <75906619+HDegroote@users.noreply.github.com>
2023-05-22 12:51:05 +0200

    Style + comment

--- index.js
+++ index.js
@@ -226,7 +226,8 @@ module.exports = class RandomAccessFile extends RandomAccessStorage {
     fs.unlink(this.filename, onunlink)
 
     function onunlink (err) {
-      if (err?.code === 'ENOENT') err = null
+      // if the file isn't there, its already unlinked, ignore
+      if (err && err.code === 'ENOENT') err = null
 
       if (!self._rmdir || !root || dir === root) return req.callback(err)
       fs.rmdir(dir, onrmdir)
