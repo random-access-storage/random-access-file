0f82da3e50a8dbe4017342d490afcd2ab4d48c24
Mathias Buus <mathiasbuus@gmail.com>
2023-06-08 21:26:07 +0200

    also test for readonly sparse

--- test/basic.js
+++ test/basic.js
@@ -502,6 +502,24 @@ test('readonly mode', function (t) {
   })
 })
 
+test('readonly mode + sparse', function (t) {
+  t.plan(1)
+
+  const filename = gen()
+  const f = new RAF(filename, { sparse: true })
+
+  f.write(0, Buffer.from('hello world'), function () {
+    f.close(function () {
+      const r = new RAF(filename, { writable: false, sparse: true })
+
+      r.read(0, 5, function (_, data) {
+        t.alike(data, Buffer.from('hello'))
+        r.close()
+      })
+    })
+  })
+})
+
 function gen () {
   return path.join(tmp, ++i + '.txt')
 }
